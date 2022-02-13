42c40cdb5ea9212b3d4d3ca6d6c8049e0d6d7b14
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2022-02-13 09:55:50 -0800

    b

--- cord.cpp
+++ cord.cpp
@@ -6,8 +6,16 @@
 using namespace std;
 
 int main() {
-  string message;
-  message = "starting...";
-  cout << message;
+  string input;
+  try {
+    input = ifstream.open(argv[0], ifstream::in);
+  } catch {
+    cerr << "Could not find source file";
+    return 1;
+  }
+  input = ifstream.open(argv[0], ifstream::in);
+  for (int i = 0; i < input.gcount(); i++) {
+    cout << input.get();
+  }
   return 0;
 }
