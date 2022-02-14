f05fcb400dba489ea8fd87b0e99825e5bbf1cbd3
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2022-02-14 14:11:30 -0800

    Update and rename cord.cpp to parser.cpp

--- parser.cpp
+++ parser.cpp
@@ -7,18 +7,37 @@ using namespace std;
 
 int main() {
   string input;
+  
   try {
     input = ifstream.open(argv[0], ifstream::in);
   } catch {
     throw exception;
   }
+  
   input = ifstream.open(argv[0], ifstream::in);
   string currentLet;
   string curStr;
   string toPass[150000];
+  int ii = 0;
   for (int i = 0; i < input.gcount(); i++) {
     currentLet = input.get();
     curStr+=currentLet;
-  }
+    if ((curStr.find("//") == ii - 1)) {
+      curStr = "";
+      ii = 0;
+      while ((currentLet != M_NEWLINE) && (i < input.gcount();) {
+        currentLet = input.get();
+        i++;
+      }
+    }
+    if ((curStr.find("/*") == ii - 1)) {
+      while (() && (i < input.gcount()) {
+        currentLet = input.get();
+        i++;
+      }
+      curStr = "";
+      ii = 0;
+    }
+    ii++;
   return 0;
 }
